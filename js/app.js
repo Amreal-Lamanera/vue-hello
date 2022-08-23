4a95c62d135a97977a59ae371813d6e730e01aa1
Amreal-Lamanera <pieraccini.francesco@gmail.com>
2022-08-23 14:51:52 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            titolo: 'Lama Portfolio',
+            alt: 'Lama logo',
+            src: '../lama.PNG',
+            logo: 'logo',
+            link: 'https://lama-homepage.netlify.app/',
+            titleClass: 'main-title',
+            aClass: 'my-link',
+            zoomClass: ''
+        },
+        methods: {
+            zoom() {
+                this.zoomClass = 'zoom'
+            },
+            unZoom() {
+                this.zoomClass = ''
+            }
+        },
+    }
+);
